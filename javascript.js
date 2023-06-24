edfaa74af2a915d5ff861bb25671745241eec00f
Yamura3 <96625893+Yamura3@users.noreply.github.com>
2023-06-24 17:30:05 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,25 +1,6 @@
-// Function to simulate sending an email
-function sendEmail(formData) {
-    // Simulate sending an email
-    console.log("Sending email...");
-    console.log("To: marceldoethetweer.be@gmail.com");
-    console.log("Subject: New Contact Message");
-    console.log("Name:", formData.get("name"));
-    console.log("Email:", formData.get("email"));
-    console.log("Message:", formData.get("message"));
-    console.log("Email sent successfully!");
-}
+const skillElements = document.querySelectorAll('.skill-progress');
 
-// Handle form submission
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Get form data
-    var formData = new FormData(event.target);
-
-    // Simulate sending email
-    sendEmail(formData);
-
-    // Reset form fields
-    event.target.reset();
+skillElements.forEach(skillElement => {
+  const progress = skillElement.getAttribute('data-progress');
+  skillElement.style.setProperty('--progress', progress / 100);
 });
